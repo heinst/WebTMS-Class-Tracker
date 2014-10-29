f3542b988535c31a3c8b2aab72002fe455bb93c9
Trevor Heins <heinstrevorj@gmail.com>
2014-10-29 01:09:08 -0400

    Disabled Add button when WebTMS down
    Signed-off-by: Trevor Heins <heinstrevorj@gmail.com>

--- WebTMS Class Tracker/ViewController.h
+++ WebTMS Class Tracker/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *addButton;
+@property (strong, nonatomic) IBOutlet UITableView *mainTable;
 
 @end
 
